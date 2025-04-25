f8017f888c929293b6e4c2f6669be71c6d301475
朽朽朽朽朽 <694253220@qq.com>
2025-04-26 03:01:02 +0800

    保存装饰器结构

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { Plugin } from "obsidian";
-import { RangeSetBuilder, EditorState } from '@codemirror/state';
+import { Prec, RangeSetBuilder, EditorState } from '@codemirror/state';
 import { keymap, EditorView, Decoration, ViewPlugin, ViewUpdate } from '@codemirror/view';
 import { GenericWidget } from "./GenericWidget";
 import { CustomRenderSettingTab } from "./setting";
+
+//@ts-ignore
+import { syntaxTree } from '@codemirror/language';
 
 export interface IRule {
 	pattern: string;
@@ -48,7 +51,7 @@ export default class CustomRendererPlugin extends Plugin {
 	init() {
 		let self = this;
 
-		//初始化需要回车复制行的前缀
+		// 初始化需要回车复制行的前缀
 		this.settings.replaceRules.forEach(rule => {
 			let patterns = rule.pattern.split("text");
 			if (!patterns[1]) {
@@ -59,7 +62,6 @@ export default class CustomRendererPlugin extends Plugin {
 			}
 		});
 
-		// 记录上一次在标记内的光标
 		let lastCursorInside: number | null = null;
 		let isInside = false;
 
@@ -69,17 +71,24 @@ export default class CustomRendererPlugin extends Plugin {
 			isMouseUp = true;
 		});
 
-		let rules = [this.closeRules, this.openRules];
+		let rules = [this.openRules, this.closeRules];
 
 		const CustomReplacePlugin = ViewPlugin.fromClass(
 			class {
 				decorations;
+				state: EditorState;
+
+				resetMark: boolean = false;
 
 				constructor(view: EditorView) {
+					// this.state = view.state;
 					this.decorations = this.buildDecorations(view);
 				}
 
 				update(update: ViewUpdate) {
+					if (this.resetMark) {
+						return;
+					}
 
 					if (update.geometryChanged ||
 						update.docChanged ||
@@ -87,6 +96,7 @@ export default class CustomRendererPlugin extends Plugin {
 						update.viewportChanged ||
 						update.focusChanged) {
 
+						this.state = update.state;
 						this.decorations = this.buildDecorations(update.view);
 						isMouseUp = false;
 					}
@@ -94,6 +104,47 @@ export default class CustomRendererPlugin extends Plugin {
 
 				buildDecorations(view: EditorView) {
 					const builder = new RangeSetBuilder<Decoration>();
+
+					const tree = syntaxTree(view.state);
+					tree.iterate({
+						enter: (node: any) => {
+							// 假设我们只想装饰 "FunctionDeclaration"
+							// if (node.name === "FunctionDeclaration") {
+							// 	const parent = node.node.parent;
+							// 	if (!parent || parent.name !== "FunctionDeclaration") {
+							// 		// 只有在没有父节点是 FunctionDeclaration 时才装饰（即最上层）
+							// 		builder.add(node.from, node.to, Decoration.mark({ class: "my-deco" }));
+							// 	}
+							// }
+							console.log(node.name);
+						}
+					});
+
+
+					// const spans = Array.from(view.dom.querySelectorAll('span'));
+
+					// self.delayedFrames(() => {
+					// 	this.resetMark = true;
+					// 	for (let span of spans) {
+					// 		span.setAttribute('data-resetMark', 'true');
+					// 	}
+					// 	this.resetMark = false;
+					// }, 30)
+
+					// console.log(spans.length);
+					// const clonedSpans = spans.map(span => {
+					// 	span.setAttribute('data-resetMark', 'true');
+					// 	let node = span.cloneNode(true) as HTMLElement;
+					// 	console.log("克隆节点", node.textContent)
+					// 	return node;
+					// });
+
+					// self.delayedFrames(() => {
+					// 	let reses = Array.from(view.dom.querySelectorAll('span'));
+					// 	for (let res of reses) {
+					// 		console.log(res.textContent, res);
+					// 	}
+					// }, 1)
 
 					if (!self.isLivePreview()) return builder.finish();
 
@@ -103,129 +154,182 @@ export default class CustomRendererPlugin extends Plugin {
 					let isInsideTemp = false;
 
 					// 收集所有匹配结果并排序
-					const matches: Array<{
-						matchFrom: number,
-						matchTo: number,
-						contentFrom: number,
-						contentTo: number,
-						rule: IRule,
-						isSuffix: boolean,
-						index: number
-					}> = [];
+					// const matches: Array<{
+					// 	matchFrom: number,
+					// 	matchTo: number,
+					// 	contentFrom: number,
+					// 	contentTo: number,
+					// 	rule: IRule,
+					// 	isSuffix: boolean,
+					// 	index: number
+					// }> = [];
+
+					let decos = [];
 
 					for (let { from, to } of view.visibleRanges) {
 						const text = view.state.doc.sliceString(from, to);
 						for (const child of rules) {
 							for (const rule of child) {
 								let match;
-
 								let patterns = rule.pattern.split("text");
 								let pattern = self.generateRegex(patterns[0], patterns[1]);
-
 								const isSuffix = !!patterns[1];
 
 								while ((match = pattern.exec(text)) !== null) {
-									//匹配边界
 									const matchFrom = from + match.index;
 									const matchTo = matchFrom + match[0].length;
-
-									//内容边界
 									const contentFrom = matchFrom + patterns[0].length;
 									const contentTo = matchTo - (isSuffix ? patterns[1].length : 0);
 
+									// matches.push({
+									// 	matchFrom,
+									// 	matchTo,
+									// 	contentFrom,
+									// 	contentTo,
+									// 	rule,
+									// 	isSuffix,
+									// 	index: match.index
+									// });
+									const isAnyContent = contentFrom < contentTo;
+									if (!isAnyContent) continue;
 
-									matches.push({
-										matchFrom,
-										matchTo,
-										contentFrom,
-										contentTo,
-										rule,
-										isSuffix,
-										index: match.index
-									});
+									const isCursorInside = cursor >= matchFrom && cursor <= matchTo;
+									const isCursorLeft = cursor <= contentFrom && cursor > matchFrom;
+									const isCursorRight = cursor >= contentTo && cursor < matchTo;
+									const isSelection = selection.from <= matchFrom && selection.to >= matchTo;
 
+									if (view.hasFocus && (isCursorInside || isSelection)) {
 
+										decos.push({
+											from: matchFrom,
+											to: contentFrom,
+											deco: Decoration.mark({
+												class: "sign-mark"
+											})
+										});
+										decos.push({
+											from: matchFrom,
+											to: matchTo,
+											deco: Decoration.mark({
+												class: rule.css,
+												attributes: {
+													'data-deco': "true",
+												}
+											})
+										});
+
+										if (isSuffix) {
+
+											decos.push({
+												from: contentTo,
+												to: matchTo,
+												deco: Decoration.mark({
+													class: "sign-mark"
+												})
+											});
+										}
+
+										if (!isInside || match.index != lastCursorInside) {
+											if (isCursorLeft) {
+												requestAnimationFrame(() => {
+													const selection = { anchor: matchFrom, head: matchFrom };
+													view.dispatch({
+														selection: selection,
+													});
+												});
+											} else if (isCursorRight) {
+												requestAnimationFrame(() => {
+													const selection = { anchor: matchTo, head: matchTo };
+													view.dispatch({
+														selection: selection,
+													});
+												});
+											}
+
+											lastCursorInside = match.index;
+										}
+
+										isInsideTemp = true;
+									} else {
+
+										decos.push({
+											from: matchFrom,
+											to: contentFrom,
+											deco: Decoration.replace({
+												widget: new GenericWidget("", "span", []),
+											})
+										});
+
+										decos.push({
+											from: matchFrom,
+											to: matchTo,
+											deco: Decoration.mark({
+												class: rule.css,
+												attributes: {
+													'data-deco': "true",
+												}
+											})
+										})
+
+										if (isSuffix) {
+
+											decos.push({
+												from: contentTo,
+												to: matchTo,
+												deco: Decoration.replace({
+													widget: new GenericWidget("", "span", []),
+												})
+											});
+										}
+									}
 								}
 							}
 						}
-
 					}
 
-					// 按 from 位置排序
-					matches.sort((a, b) => a.matchFrom - b.matchFrom);
+					decos.sort((a, b) => a.from - b.from);
 
-					for (const match of matches) {
-						const { matchFrom, matchTo, contentFrom, contentTo, rule, isSuffix } = match;
-						//状态判断
-						const isAnyContent = contentFrom < contentTo;
-
-						//没有内容就默认显示
-						if (!isAnyContent) continue;
-
-						const isCursorInside = cursor >= matchFrom && cursor <= matchTo; // 光标是否在匹配内容内
-						const isCursorLeft = cursor <= contentFrom && cursor > matchFrom; // 光标是否在匹配内容内且在实际内容左边
-						const isCursorRight = cursor >= contentTo && cursor < matchTo; // 光标是否在匹配内容内且在实际内容右边
-						const isSelection = selection.from <= matchFrom && selection.to >= matchTo; // 选取是否包含匹配内容
-
-						//当前编辑器是焦点且 光标在匹配内容内或者选取包括匹配内容 的情况下，还原分割好的 md
-						//否则渲染为无 md 符号的样式
-						if (view.hasFocus && (isCursorInside || isSelection)) {
-							builder.add(matchFrom, contentFrom, Decoration.mark({
-								class: "sign-mark"
-							}));
-
-							builder.add(matchFrom, matchTo, Decoration.mark({
-								class: rule.css
-							}));
-
-							if (isSuffix) {
-								builder.add(contentTo, matchTo, Decoration.mark({
-									class: "sign-mark"
-								}));
-							}
-
-							// console.log(match.index, lastCursorInside?.index, isInside, isInsideTemp)
-							if (!isInside || match.index != lastCursorInside) {
-								//移动光标到首尾
-								if (isCursorLeft) {
-									requestAnimationFrame(() => {
-										const selection = { anchor: matchFrom, head: matchFrom };
-										view.dispatch({
-											selection: selection,
-										});
-									});
-								} else if (isCursorRight) {
-									requestAnimationFrame(() => {
-										const selection = { anchor: matchTo, head: matchTo };
-										view.dispatch({
-											selection: selection,
-										});
-									});
-								}
-
-								lastCursorInside = match.index;
-							}
-
-							isInsideTemp = true;
-
-						} else {
-							builder.add(matchFrom, contentFrom, Decoration.replace({
-								widget: new GenericWidget("", "span", []),
-								inclusive: false
-							}));
-
-							builder.add(matchFrom, matchTo, Decoration.mark({
-								class: rule.css
-							}));
-
-							if (isSuffix) {
-								builder.add(contentTo, matchTo, Decoration.replace({
-									widget: new GenericWidget("", "span", []),
-									inclusive: false
-								}));
-							}
-						}
+					for (let deco of decos) {
+						builder.add(deco.from, deco.to, deco.deco);
 					}
+
+					// self.delayedFrames(() => {
+					// 	this.resetMark = true;
+					// 	const newSpans = Array.from(view.dom.querySelectorAll('span[data-deco="true"]'));
+
+					// 	for (let newSpan of newSpans) {
+					// 		// newSpan.removeAttribute('data-resetMark');
+					// 		let parent: Element | null = newSpan;
+					// 		console.log("父节点", parent)
+					// 		while (parent) {
+					// 			let parentNode = parent.parentElement;
+					// 			if (parentNode?.hasAttribute('data-resetMark')) {
+					// 				// console.log("移除节点", newSpan)
+					// 				parentNode.innerHTML = parent.innerHTML;
+					// 				// console.log(parentNode.innerHTML, parent.innerHTML)
+					// 				break;
+					// 			}
+					// 			parent = parent.parentElement;
+					// 		}
+					// 	}
+
+					// 	// console.log("采集节点", clonedSpans)
+					// 	// for (let newSpan of newSpans) {
+					// 	// 	let node = clonedSpans.shift() as HTMLElement;
+					// 	// 	if (node.textContent !== newSpan.textContent) continue;
+
+					// 	// 	// console.log("还原节点", newSpan.parentElement)
+					// 	// 	// newSpan.replaceWith(node);
+					// 	// 	// newSpan.parentElement?.remove();
+					// 	// 	// newSpan.nextElementSibling?.nextElementSibling?.remove();
+					// 	// 	// newSpan.nextElementSibling?.remove();
+					// 	// 	// newSpan.previousElementSibling?.previousElementSibling?.remove();
+					// 	// 	// newSpan.previousElementSibling?.remove();
+					// 	// 	// newSpan.remove();
+					// 	// }
+					// 	this.resetMark = false;
+
+					// }, 60);
 
 					isInside = isInsideTemp;
 
@@ -237,44 +341,32 @@ export default class CustomRendererPlugin extends Plugin {
 			}
 		);
 
-		// 注册 Editor 扩展
-		this.registerEditorExtension(CustomReplacePlugin);
+		this.registerEditorExtension(
+			Prec.lowest(CustomReplacePlugin)  // 关键修改点
+		);
 
-		// 在插件的 onload() 方法中添加：
 		this.registerDomEvent(document, 'keydown', (evt) => {
 			if (evt.key === 'Enter') {
-				// 获取当前活动编辑器
 				const activeEditor = this.app.workspace.activeEditor?.editor;
 				if (!activeEditor) return;
 
-				// 获取当前行内容
 				const cursor = activeEditor.getCursor();
 				const line = cursor.line - 1;
 				const lineContent = activeEditor.getLine(line);
 
-				// console.log("捕获到回车键，当前行内容:", lineContent, cursor);
-
 				let matchedPrefix = '';
-
 				this.copyLinePrefix.some(prefix => lineContent.startsWith(prefix) && (matchedPrefix = prefix));
-				console.log("匹配到的前缀:", this.copyLinePrefix, matchedPrefix, lineContent);
 
 				if (matchedPrefix) {
 					evt.preventDefault();
 
-					console.log(lineContent === matchedPrefix && lineContent.length === matchedPrefix.length)
 					if (lineContent === matchedPrefix && lineContent.length === matchedPrefix.length) {
-
-						// 直接删除整行（模拟普通回车行为）
 						activeEditor.replaceRange(
 							'',
 							{ line: line, ch: 0 },
 							{ line: line, ch: matchedPrefix.length }
 						);
-						console.log("删除整行");
-					}
-					// 情况2：正常注释行
-					else {
+					} else {
 						activeEditor.replaceRange(
 							matchedPrefix,
 							{ line: line + 1, ch: 0 },
@@ -285,6 +377,34 @@ export default class CustomRendererPlugin extends Plugin {
 				}
 			}
 		});
+
+		this.registerMarkdownPostProcessor((el, ctx) => {
+			for (const child of rules) {
+				for (const rule of child) {
+					const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT);
+					let node: Text;
+
+					let patterns = rule.pattern.split("text");
+					let pattern = self.generateRegex(patterns[0], patterns[1]);
+
+					while ((node = walker.nextNode() as Text)) {
+						let nodeValue = node.nodeValue as string;
+						if (pattern.test(nodeValue)) {
+
+							const spanified = nodeValue.replace(pattern, (_match, p1) => {
+								return `<span class="${rule.css}">${p1}</span>`;
+							});
+
+							const tempDiv = this.app.workspace.containerEl.createDiv();
+							tempDiv.innerHTML = spanified;
+
+							node.replaceWith(...Array.from(tempDiv.childNodes));
+						}
+					}
+				}
+			}
+		});
+
 
 	}
 
@@ -302,13 +422,31 @@ export default class CustomRendererPlugin extends Plugin {
 		let pattern;
 		if (suffix) {
 			const escapedSuffix = escape(suffix);
-			// 注意：不支持嵌套，仅匹配最简单的非后缀内容
-			pattern = `${escapedPrefix}([^${suffix[0]}\r\n\t ]*)${escapedSuffix}`;
+			pattern = `${escapedPrefix}([^${suffix[0]}\r\n\t]*)${escapedSuffix}`;
 		} else {
 			pattern = `${escapedPrefix}(.*?)(\r?\n|$)`;
 		}
 
 		return new RegExp(pattern, 'g');
+	}
+
+	delayedFrames(callback: Function, delay: number = 2) {
+		let frameCount = 0;
+		function run() {
+			// 增加帧计数
+			frameCount++;
+
+			// 如果已经过了两帧，则执行目标操作
+			if (frameCount === delay) {
+				callback();
+			} else {
+				// 否则继续请求下一帧
+				requestAnimationFrame(run);
+			}
+		}
+
+		// 开始请求第一帧
+		requestAnimationFrame(run);
 	}
 
 	async loadSettings() {
